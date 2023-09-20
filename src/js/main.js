25b899eb431728f7037f435d467d9ee72e70360a
Amil4567 <khdrv427@gmail.com>
2023-09-20 20:39:15 +0400

    Task

---
+++ src/js/main.js
@@ -0,0 +1,56 @@
+// function DarkMode() {
+//     document.getElementById("lamp").style.filter = ("grayscale(1)");
+// }
+
+// function LightMode() {
+//     document.getElementById("lamp").style.filter = ("grayscale(0)");
+// }
+
+
+
+// let gun=17;
+// ay=5;
+// il=2001;
+
+// console.log(gun,ay,il);
+
+
+
+// let treli = adlar.join("-");
+// console.log(treli);
+
+// let vergullu = adlar.join(",");
+
+// console.log(vergullu);
+
+// console.log(treli);
+
+// console.log(adlar);
+
+// console.log(adlar);
+
+
+
+// adlar.pop();
+// console.log(adlar);
+
+
+let age = prompt('nece yasiniz var?');
+if (age >= 18){
+    alert('Giriş təsdiqləmdi');
+}
+else{
+    alert('18 yaşdan kiçik şəxslərə giriş qaadağan!');
+}
+
+
+
+
+
+function changeBackgrounColor() {
+    document.getElementById('bGC').style.backgroundColor = 'black';
+}
+
+confirm("Arxa fon rəngini dəyişdirmək istədiyinizə əminsiniz?");
+
+changeBackgrounColor();
